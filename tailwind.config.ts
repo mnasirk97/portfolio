a31aaba3a249f1f35718d23c9258a3829a137f51
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-04-29 23:37:06 +0500

    Add initial project setup with README, Tailwind CSS, PostCSS, and TypeScript configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: "class", // 👈 important for light/dark theme toggle
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
+
+// import type { Config } from "tailwindcss";
+
+// const config: Config = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
